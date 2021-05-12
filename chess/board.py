45e996b70100f3518c6b900d03e030570fdab71d
ibbeyo <ibbeyo94@gmail.com>
2021-05-12 01:59:27 -0400

    added castle

--- chess/board.py
+++ chess/board.py
@@ -1,8 +1,12 @@
 import pygame
-from typing import Tuple, List, Dict, Union
+from typing import Tuple, List, Dict, cast
 from dataclasses import dataclass
 
-from .pieces import Pawn, Rook, Bishop, King, Knight, Queen, WHITE_PIECE, BLACK_PIECE
+
+from .pieces import (
+    Piece, Pawn, Rook, Bishop, King, Knight, Queen, 
+    WHITE_PIECE, BLACK_PIECE
+)
 
 
 WINDOW_WIDTH, WINDOW_HEIGHT = (480, 480)
@@ -13,24 +17,22 @@ BOARD_IMAGE = pygame.image.load('./images/board.jpg')
 POSSIBLE_MOVES_BORDER = pygame.Surface((60,60))
 POSSIBLE_MOVES_BORDER.set_alpha(128)
 
-PIECE_TYPES = Union[Pawn, Rook, Bishop, King, Knight, Queen]
-
+EVENT_MOVE = 0
+EVENT_CAPTURE = 1
+EVENT_ENPASSANT = 2
+EVENT_CASTLE = 3
+EVENT_PROMOTION = 4
 
 @dataclass
 class MoveLog:
-    from_node   : Tuple[int, int]
-    to_node     : Tuple[int, int]
+    from_pos    : Tuple[int, int]
+    to_pos      : Tuple[int, int]
     from_tile   : Tuple[int, int]
     to_tile     : Tuple[int, int]
-    piece       : PIECE_TYPES
-    event       : str
-
-
-@dataclass
-class Node:
-    piece: PIECE_TYPES
-    tile : str
-
+    piece       : Piece or None
+    capture     : Piece or None
+    castle      : Piece or None
+    event       : int
 
 class Board:
 
@@ -39,7 +41,8 @@ class Board:
         self.board.fill((220,220,220))
 
         self.logger: List[MoveLog] = list()
-        self.nodes: Dict[Tuple[int,int], Node] = dict()
+        self.tiles: Dict[str, Piece] = dict()
+        self.tiles_position: Dict[str, Tuple[int,int]] = dict()
         self.valid_moves: list = list()
 
         self.piece = None
@@ -60,7 +63,7 @@ class Board:
 
         tiles = [[c+str(r) for c in cols] for r in rows]
 
-        starting_positions = {
+        self.starting_positions = {
             Pawn    : tiles[1] + tiles[6],
             Rook    : ['a1', 'a8', 'h1', 'h8'],
             Bishop  : ['c1', 'c8', 'f1', 'f8'],
@@ -71,68 +74,70 @@ class Board:
 
         for x, tile_group in enumerate(tiles):
             for y, tile in enumerate(tile_group):
-                node_position = (60*y,60*x)
-                self.nodes.update({node_position: Node(piece=None, tile=tile)})
+                position = (60*y,60*x)
+                self.tiles.update({tile: None})
+                self.tiles_position.update({position: tile})
 
-                for piece, placement in starting_positions.items():
+                for piece, placement in self.starting_positions.items():
                     if tile in placement:
                         color = BLACK_PIECE if int(tile[1:]) >= 7 else WHITE_PIECE
-                        piece = piece(tile=tile, color=color, position=node_position)
-                        self.board.blit(piece.load_image, node_position)
-                        self.nodes[node_position].piece = piece
-
-        self.node_positions = list(self.nodes.keys())
+                        piece = piece(tile=tile, color=color, position=position)
+                        self.board.blit(piece.load_image, position)
+                        self.tiles[tile] = piece
 
     
     def select(self) -> None:
-        node_position = self._get_nearest_node_pos()
-        self.piece = self.nodes[node_position].piece
+        tile, _ = self._get_event()
+        self.piece = self.tiles[tile]
 
         if self.piece:
             self.piece_is_moving = True
             self.move_validation()
             self.refresh()
-            self.nodes[node_position].piece = None
+            self.tiles[tile] = None
 
             
     def release(self) -> None:
-        if self.piece:
-            node_position = self.release_validation()
+        event = EVENT_MOVE
+        capture = None
+        castle = None
 
-            if not node_position:
+        if self.piece:
+            tile, position = self._get_event()
+            validated = self.release_validation(tile, position)
+            
+            if not validated:
                 self.reset_piece_location()
 
-            elif node_position:
-                to_tile = self.nodes[node_position].tile
+            elif validated:
 
-                if self.has_en_passant:
-                    self.nodes[self.logger[-1].piece.position].piece = None
+                if isinstance(self.piece, Pawn) and self.has_en_passant:
+                    self.tiles[self.logger[-1].piece.tile] = None
+                    event = EVENT_ENPASSANT
+                    capture = self.logger[-1].piece
 
-                self.logger.append(
-                    MoveLog(
-                        from_node   =self.piece.position, 
-                        to_node     =node_position, 
-                        from_tile   =self.piece.tile, 
-                        to_tile     =to_tile, 
-                        piece       =self.piece,
-                        event       ='move'
-                    )
-                )
 
-                self.nodes[self.piece.position].piece = None
+                if isinstance(self.piece, King):
+                    if self.piece.can_castle():
+                        event = EVENT_CASTLE
+                        capture, castle = self.castle(tile[:1])
+
+                self.log(position, tile, capture, event, castle)
+
+                self.tiles[self.piece.tile] = None
 
                 if not self.piece.has_moved:
                     self.piece.has_moved = True
 
-                self.piece.position = node_position
-                self.piece.tile = to_tile
-                self.nodes[node_position].piece = self.piece
+                self.piece.position = position
+                self.piece.tile = tile
+                self.tiles[tile] = self.piece
 
         self.reset_board_movement()
         self.refresh()
 
 
-    def release_validation(self) -> Tuple[int, int] or bool:
+    def release_validation(self, tile, position) -> bool:
         '''
         Validates the following:
         1. That a piece can only take of the opposite color.
@@ -141,15 +146,14 @@ class Board:
         4. That its the corret color to move.
         '''
     
-        node_position = self._get_nearest_node_pos()
         if any([
             self.whos_turn() != self.piece.color,
-            self.piece == self.nodes[node_position].piece,
-            node_position not in self.valid_moves
+            self.piece == self.tiles[tile],
+            position not in self.valid_moves
         ]):
             return False
 
-        return node_position
+        return True
 
 
     def move(self) -> None:
@@ -163,47 +167,49 @@ class Board:
    
         for motion, positions in enumerate(self.piece.get_moves().values()):
             for pos in positions:
-                if pos in self.node_positions:
+                tile = self.tiles_position.get(pos)
+                if tile:
                     self.valid_moves.append(pos)
 
-                    if other := self.nodes[pos].piece:
+                    if other := self.tiles[tile]:
                         
                         if self.piece.color == other.color:
                             self.valid_moves.remove(pos)
 
-                            #allow knight to jump
-                            if not isinstance(self.piece, Knight):
+                            if not isinstance(self.piece, Knight): #allow knight to jump over same color
                                 break
 
                         elif self.piece.color != other.color:
 
-                            #pawn cant take piece in front of it
-                            if isinstance(self.piece, Pawn) and motion == 2:
+                            if isinstance(self.piece, Pawn) and motion == 2: #pawn cant take piece in front of it
                                 self.valid_moves.remove(pos)
                                 break
                             
-                            #allow knight to jump
-                            if not isinstance(self.piece, Knight):
+                            if not isinstance(self.piece, Knight): #allow knight to jump over diff color
                                 break
                     
-                    #dont allow pawn to move diagonal on empty node
-                    elif isinstance(self.piece, Pawn) and motion < 2:
-                        self.valid_moves.remove(pos)
-                        if passant := self.en_passant():
-                            self.valid_moves.append(passant)
+                    elif isinstance(self.piece, Pawn) and motion < 2: #dont allow pawn to move diagonal on empty node
+                        if pos != self.en_passant(): #check for en_passant
+                            self.valid_moves.remove(pos)
 
-                    
 
     def undo_move(self) -> None:
         if self.logger:
             move = self.logger[-1]
-            self.nodes[move.to_node].piece = None
 
-            piece = move.piece
-            piece.position = move.from_node
-            piece.tile = move.from_tile
+            if move.from_tile in sum(list(self.starting_positions.values()), []):
+                move.piece.has_moved = False
 
-            self.nodes[move.from_node].piece = piece
+            move.piece.position = move.from_pos
+            move.piece.tile = move.from_tile
+            self.tiles[move.from_tile] = move.piece
+
+            if move.capture:
+                self.tiles[move.capture.tile] = move.capture
+                if move.event == EVENT_CASTLE:
+                    self.tiles[move.castle.tile] = None
+
+            self.tiles[move.to_tile] = None
 
             del self.logger[-1]
 
@@ -211,7 +217,7 @@ class Board:
 
 
     def reset_piece_location(self) -> None:
-        self.nodes[self.piece.position].piece = self.piece
+        self.tiles[self.piece.tile] = self.piece
 
         
     def reset_board_movement(self) -> None:
@@ -233,9 +239,9 @@ class Board:
 
     def refresh(self) -> None:
         self.__draw_board()
-        for node in self.nodes.values():
-            if node.piece:
-                self.board.blit(node.piece.load_image, node.piece.position)
+        for node in self.tiles.values():
+            if isinstance(node, Piece):
+                self.board.blit(node.load_image, node.position)
 
         if self.valid_moves:
             for area in self.valid_moves:
@@ -246,17 +252,53 @@ class Board:
     def en_passant(self) -> bool:
         if self.logger and self.whos_turn() == self.piece.color:
             previous_move = self.logger[-1]
-            if isinstance(previous_move.piece, Pawn) and previous_move.piece != self.piece:
 
-                if previous_move.from_node[1] == previous_move.piece.starting_position:
+            if isinstance(previous_move.piece, Pawn):
+                if previous_move.from_pos[1] == previous_move.piece.starting_position:
                     passant = getattr(previous_move.piece, previous_move.piece._reverse_orient)(1)
+                    
                     if any([
-                        self.piece.left(1) == previous_move.to_node, 
-                        self.piece.right(1) == previous_move.to_node
+                        self.piece.left(1) == previous_move.to_pos, 
+                        self.piece.right(1) == previous_move.to_pos
                     ]):
                         self.has_en_passant = True
                         return passant
         return False
+
+
+    def castle(self, file) -> Tuple[Rook, Rook] or Tuple[None, None]:
+        direction = 'right' if file == 'g' else 'left'
+        distance = 3 if direction == 'right' else 4
+        movefunc = getattr(self.piece, direction)
+
+        rook = self.tiles[self.tiles_position[movefunc(distance)]]
+        prev_rook = Rook(rook.tile, rook.color, rook.position)
+
+        if rook and rook.can_castle():
+            self.tiles[rook.tile] = None
+            rook.position = movefunc(1)
+            rook.tile = self.tiles_position[rook.position]
+            self.tiles[rook.tile] = rook
+            return prev_rook, rook
+        return None, None
+
+
+    def log(self, to_position: Tuple[int, int], to_tile: Tuple[int, int], 
+        capture: Piece = None, event: int = 0, castle: Piece = None):
+
+        self.logger.append(
+            MoveLog(
+                from_pos    =self.piece.position, 
+                to_pos      =to_position, 
+                from_tile   =self.piece.tile, 
+                to_tile     =to_tile, 
+                piece       =self.piece,
+                capture     =capture,
+                castle      =castle,
+                event       =event
+            )
+        )
+        print(self.logger[-1])
 
 
 
@@ -264,12 +306,12 @@ class Board:
         self.board.blit(BOARD_IMAGE, BOARD_OFFSET)
 
 
-    def _get_nearest_node_pos(self) -> Tuple[int, int] or None:
+    def _get_event(self) -> Tuple[str, Tuple[int, int]] or None:
         x, y = self.event_pos
-        for pos in self.node_positions:
+        for pos, tile in self.tiles_position.items():
             if x in range(pos[0] + 60) and y in range(pos[1] + 60):
-                return pos
-        return None
+                return tile, pos
+        return None, None
             
 
     
